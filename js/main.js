c73a3426fc65d40261a97eb47b5f6676bd7714fe
emdsgn <Monica@Thetinyone.local>
2014-01-30 08:01:20 -0500

    Fixed pop up issues

--- js/main.js
+++ js/main.js
@@ -30,3 +30,11 @@ $('.previous').on('click', function () {
 
   switchItems(current, previous);
 });
+
+var dialog = document.querySelector('dialog');
+document.querySelector('#show').onclick = function() {
+  dialog.show();
+};
+document.querySelector('#close').onclick = function() {
+  dialog.close();
+};
