b90ef956491bec757df8c05479ee926bc04acb9b
emdsgn <Monica@Thetinyone.local>
2014-02-13 10:40:43 -0500

    Fixed ajax bug

--- js/ajax.js
+++ js/ajax.js
@@ -6,7 +6,7 @@ var testiData = $.ajax('testimonials.json', {
 
 // Because AJAX takes time to download things of the internet .done() will be triggered only after jQuery has finished downloading.
 testiData.done(function (data) {
-	var testi = Math.round(Math.random() * data.length - 1);
+	var testi = Math.round(Math.random() * (data.length - 1) );
 console.log(data.length, testi);
 	$('.testi-name').html(data[testi].name);
 	$('.testi-quote').html(data[testi].quote);
